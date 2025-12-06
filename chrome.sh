2f4524c3c299bbc78ef93abf355d7c06f5408a56
64LONG <64298723+64LONG@users.noreply.github.com>
2025-12-06 11:45:17 -0600

    Updating with installing command using the google package

--- chrome.sh
+++ chrome.sh
@@ -3,3 +3,7 @@
 echo "Downloading Google Chrome"
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+echo "Installing Google Chrome"
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
