8c6136a9ff4214518ed10ff4deb75d191c45427f
64LONG <64298723+64LONG@users.noreply.github.com>
2025-12-06 11:33:13 -0600

    Create LinuxPerform.sh
    Script to showcase a snapshot of performance within a Linux OS.

---
+++ LinuxPerform.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+echo "Current CPU utilization of system"
+top | head -n 3 | tail -n 1
+
+echo "Current MEM utilization of system"
+free -h
+
+echo "Current space utilization of file system
+df -h
+
+echo "Top 5 processes with highest CPU usage"
+px aux --sort=-%cpu | head -n 11
+
+echo "Top 5 processes with highest MEM usage"
+px aux --sort=-%mem | head -n 11
